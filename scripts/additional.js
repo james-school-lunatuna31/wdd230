35c44cb2ff81ea63ea6ecc3d43f96499559b0f24
James Green <github.byui.school@ckplace.org>
2023-11-10 00:43:32 -0500

    dark mode and hamburgers

--- scripts/additional.js
+++ scripts/additional.js
@@ -16,8 +16,6 @@ document.querySelectorAll('.learning-info-item a').forEach(function (a) {
         a.style.color = '#a32e2e';
         a.style.textDecoration = 'underline';
         a.setAttribute('title', 'under construction');
-    } else {
-        a.style.color = 'blue';
     }
 });
 const active = document.querySelector('.active');
@@ -25,5 +23,23 @@ const active = document.querySelector('.active');
 active.removeAttribute('href'); // kill the users ability to re-load the page by clicking "Home"
 active.style.pointerEvents = 'none'; // Remove the highlighting on the active tab when the user mouses over.
 
+document.querySelector('.hamburger').addEventListener('click', function () {
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.classList.toggle('open');
+    if (navMenu.classList.contains('open')) {
+        this.textContent = 'X';
+    } else {
+        this.textContent = '☰';
+    }
+});
 
-
+const d_mode = document.getElementById('dark-moder');
+const body = document.body;
+d_mode.addEventListener('click', () => {
+    body.classList.toggle('darkmode');
+    if (body.classList.contains('darkmode')) {
+        d_mode.textContent = "Light Mode";
+    } else {
+        d_mode.textContent = "Dark Mode";
+    }
+});
