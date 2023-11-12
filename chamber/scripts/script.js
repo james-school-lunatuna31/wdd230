ff6cc0956319e9315d10fb640bc51fbdbd25892b
James Green <github.byui.school@ckplace.org>
2023-11-11 22:07:35 -0500

    framework set

---
+++ chamber/scripts/script.js
@@ -0,0 +1,19 @@
+// Dynamic footer
+const cur_date = new Date()
+const dynamic_footer_date_element = document.getElementById('DynamicDate');
+const dyanmic_footer_last_modified_element = document.getElementById('DynamicLastModified');
+dynamic_footer_date_element.innerHTML = `&copy; ${cur_date.getFullYear()}`;
+dyanmic_footer_last_modified_element.innerHTML = `<span style="color:#fff";>Last modified</span> ${cur_date}`;
+
+//page elements
+const signup_response = "";
+const signup_response_element = document.getElementById("button-response");
+signup_response_element.textContent = signup_response;
+let sign_up_button = document.getElementById('signup').addEventListener('click', () => {
+    let join_input = document.querySelector('#join-input textarea');
+    if (join_input.value.trim() !== "") { //you could regex valid email addresses here if you wanted.
+        signup_response_element.innerHTML = "Signed Up!";
+    } else {
+        signup_response_element.innerHTML = "Invalid entry";
+    }
+});
